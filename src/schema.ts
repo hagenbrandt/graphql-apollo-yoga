1f66dc71b01f662acf97c4174f1dd478b8512632
hbrandt <h.brandt@steinberg.de>
2023-02-27 09:54:31 +0100

    refactor: Adjust schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,18 +1,6 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
-
-type Link = {
-    id: string;
-    url: string;
-    description: string;
-}
-
-const links: Link[] = [
-    {
-        id: 'link-0',
-        url: 'https://graphql-yoga.com',
-        description: 'The easiest way of setting up a GraphQL server'
-    }
-]
+import { Link } from '@prisma/client';
+import { GraphQLContext } from './context';
 
 const typeDefinitions = `
   type Query {
@@ -33,22 +21,28 @@ const typeDefinitions = `
 
 const resolvers = {
     Query: {
-      info: () => `This is the API of a Hackernews Clone`,
-      feed: () => links
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: (parent: unknown, args: {}, context: GraphQLContext) => context.prisma.link.findMany()
+    },
+    Link: {
+        id: (parent: Link) => parent.id,
+        description: (parent: Link) => parent.description,
+        url: (parent: Link) => parent.url
     },
     Mutation: {
-        postLink: (parent: unknown, args: { description: string; url: string }) => {
-            const idCount = links.length
+        async postLink(
+            parent: unknown, 
+            args: { description: string; url: string },
+            context: GraphQLContext
+            ) {
+                const newLink = await context.prisma.link.create({
+                    data: {
+                        url: args.url,
+                        description: args.description
+                    }
+                })
 
-            const link: Link = {
-                id: `link-${idCount}`,
-                description: args.description,
-                url: args.url
-            }
-
-            links.push(link)
-
-            return link
+                return newLink
         }
     }
 }
